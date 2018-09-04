0a46f163498afbb50a938002b02d02bd71eee08d
Olivier <leplus.olivier@gmail.com>
2018-09-04 09:16:58 +0200

    feat(errors): Handle errors
    Moved from console.log to botfuel-dialog Logger

--- src/adapters/assistant-adapter.js
+++ src/adapters/assistant-adapter.js
@@ -11,19 +11,20 @@ const {
   BrowseCarouselItem,
 } = require('actions-on-google');
 
-const { Adapter, MessageError } = require('botfuel-dialog');
-
 const {
   Logger,
-  UserImageMessage,
+  Adapter,
   UserTextMessage,
 } = require('botfuel-dialog');
+
+const logger = Logger('AssistantAdapter');
 
 const PORT = process.env.PORT || process.env.BOTFUEL_ADAPTER_PORT || 5000;
 
 class AssistantAdapter extends Adapter {
 
   async run() {
+    logger.debug('run');
     const app = actionssdk();
 
     app.intent('actions.intent.MAIN', conv => {
@@ -44,10 +45,11 @@ class AssistantAdapter extends Adapter {
       .use(bodyParser.json())
       .use('/webhook', app)
       .use('/static', express.static('src/static'))
-      .listen(PORT, () => console.log('run: listening on port', PORT));
+      .listen(PORT, () => logger.debug('express', `run: listening on port ${PORT}`));
   }
 
   sendResponse(botMessage, conv) {
+    logger.debug('sendResponse', JSON.stringify(botMessage));
     const { payload, type } = botMessage;
     switch (type) {
       case 'text':
@@ -70,6 +72,8 @@ class AssistantAdapter extends Adapter {
   }
 
   sendMessage(response, conv, payload) {
+    logger.debug('sendMessage', JSON.stringify(response));
+    logger.debug('sendMessage', JSON.stringify(payload));
     if (payload && payload.options && payload.options.question) {
       conv.ask(...response);
     } else {
@@ -77,7 +81,12 @@ class AssistantAdapter extends Adapter {
     }
   }
 
+  adaptActions() {
+    logger.error('adaptActions: error', 'Not supported');
+  }
+
   adaptText(payload, conv) {
+    logger.debug('adaptText', JSON.stringify(payload));
     const response = new SimpleResponse({
       speech: payload.value,
       text: payload.value
@@ -87,11 +96,11 @@ class AssistantAdapter extends Adapter {
   }
 
   adaptImage(payload, conv) {
-    console.log(payload);
+    logger.debug('adaptImage', JSON.stringify(payload));
     const image = new Image({
       url: payload.value,
     });
-    image.accessibility_text = payload.options.accessibility_text || '';
+    image.accessibility_text = payload.options.title || payload.options.accessibility_text || '';
 
     const response = new BasicCard({
       title: payload.options.title || payload.options.accessibility_text || '',
@@ -101,47 +110,66 @@ class AssistantAdapter extends Adapter {
     this.sendMessage([response], conv, payload);
   }
 
-  // TODO
-  adaptCards(payload, conv) {
-    console.log(payload);
-    const items = [];
-    for (const card of payload.value) {
+  createCard(data) {
+    logger.debug('createCard', JSON.stringify(data));
+    const urls = data.buttons.filter(i => i.type === 'link');
 
-      const urls = card.buttons.filter(i => i.type === 'link');
-      if (urls.length < 1) {
-        // throw new MessageError({
-        //   name: 'url',
-        //   message: 'Google Assistant card need a valid url'
-        // });
-      }
-      items.push(
-        new BrowseCarouselItem({
-          title: card.title || '',
-          url: urls[0].value || '',
-          description: card.description || '',
-          image: new Image({
-            url: card.image_url,
-            alt: 'Alternate text',
-          }),
-          footer: card.footer || '',
-        })
-      );
+    if (urls.length < 1) {
+      logger.error('createCard: error', 'Cards must have a link');
     }
+    const url = urls[0] || '';
 
-    const response = [
-      new SimpleResponse({
-        speech: payload.options.speech || '',
-        text: payload.options.text || ''
+    return {
+      title: data.title || '',
+      url: url.value || '',
+      description: data.description || '',
+      image: new Image({
+        url: data.image_url,
+        alt: data.description,
       }),
-      new BrowseCarousel({
-        items: items
-      })
-    ]
+      footer: data.footer || '',
+    }
+  }
+
+  adaptCards(payload, conv) {
+    logger.debug('adaptImage', JSON.stringify(payload));
+    const items = [];
+    let response = []
+
+    if (payload.value.length === 1) {
+
+      const card = new BasicCard(this.createCard(payload.value[0]));
+
+      response = [
+        new SimpleResponse({
+          speech: payload.options.speech || '',
+          text: payload.options.text || ''
+        }),
+        card
+      ]
+    } else {
+      for (const card of payload.value) {
+        items.push(
+          new BrowseCarouselItem(this.createCard(card))
+        );
+      }
+
+      response = [
+        new SimpleResponse({
+          speech: payload.options.speech || '',
+          text: payload.options.text || ''
+        }),
+        new BrowseCarousel({
+          items: items
+        })
+      ]
+    }
 
     this.sendMessage(response, conv, payload);
   }
 
   adaptQuickreplies(payload, conv) {
+    logger.debug('adaptQuickreplies', JSON.stringify(payload));
     const response = new Suggestions(...payload.value);
     this.sendMessage([response], conv, payload);
   }
